ce5d58ad576d06f910c872ea2329b61f2b6f7a31
Carla Navia <cnavia27@gmail.com>
2020-12-15 20:04:10 +0100

    navbar css done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,31 @@
 import React from "react";
-import "./Navbar.css"
+import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div>
-      <nav className="navbar nav-color" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <a className="navbar-item" href="http://localhost:3000/">
-            <h1 className="h1-nav">PHONES</h1>
-          </a>
+    <nav
+      className="navbar nav-color is-fixed-top"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="navbar-brand">
+        <a className="navbar-item" href="http://localhost:3000/">
+          <h1 className="h1-nav">PHONES</h1>
+        </a>
+      </div>
+      <div>
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <Link to="/new" className="button is-light">
+                Add new phone
+              </Link>
+            </div>
+          </div>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 }
 
