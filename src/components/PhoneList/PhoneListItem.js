90c78404567d5143507ae9a7358d6800eb0d6bb1
Carla Navia <cnavia27@gmail.com>
2020-12-15 16:47:27 +0100

    PhoneListItem css doing

--- src/components/PhoneList/PhoneListItem.js
+++ src/components/PhoneList/PhoneListItem.js
@@ -1,20 +1,32 @@
 import React, { Component } from "react";
-
+import "./PhoneListItem.css";
 
 class PhoneListItem extends Component {
   render() {
     return (
-      <div key={this.props.onePhone._id}>
-        <div>
+      <div key={this.props.onePhone._id} className="card">
+        <div className="distr-card">
+          <p className="name">{this.props.onePhone.name}</p>
           <img src={this.props.onePhone.imageFileUrl} alt="phone" />
         </div>
+        <div className="distr-card text">
         <div>
-          <p>{this.props.onePhone.name}</p>
-        </div>
-
-        <div>
-          <p>{this.props.onePhone.description}</p>
-          <p>{this.props.onePhone.screen} </p>
+          <p className="others">
+            <span className="bold-text">Screen:</span>
+            {this.props.onePhone.screen}"
+          </p>
+          <p className="others">
+            <span className="bold-text">Manufacturer:</span>
+            {this.props.onePhone.manufacturer}
+          </p>
+          <p className="others">
+            <span className="bold-text">Processor:</span>
+            {this.props.onePhone.processor}
+          </p>
+          </div>
+          <div className="price-position">
+            <p className="price">{this.props.onePhone.price},00€</p>
+          </div>
         </div>
       </div>
     );
