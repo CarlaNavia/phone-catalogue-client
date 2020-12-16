50425e2f3240d86ddd55f497f9ac561f7903511c
Carla Navia <cnavia27@gmail.com>
2020-12-16 10:56:45 +0100

    navbar - add button done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -18,8 +18,8 @@ function Navbar() {
         <div className="navbar-end">
           <div className="navbar-item">
             <div className="buttons">
-              <Link to="/new" className="button is-light">
-                Add new phone
+              <Link to="/new" className="button is-light ">
+                <p className="h1-nav add">Add new phone</p>
               </Link>
             </div>
           </div>
